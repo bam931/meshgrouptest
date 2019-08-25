1fc8992f08c053eff01da5872ecd8f9f5c28eb79
Alex <a.proskurin@myfeelix.de>
2019-08-25 14:26:56 +0300

    app-component: edit/delete components added onDestroy

--- src/app/product/product-delete/product-delete.component.ts
+++ src/app/product/product-delete/product-delete.component.ts
@@ -1,7 +1,9 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { ProductService } from '../product.service';
+import { Subject } from 'rxjs/index';
+import { takeUntil } from 'rxjs/internal/operators';
 
 
 interface DialogData {
@@ -13,7 +15,8 @@ interface DialogData {
   templateUrl: './product-delete.component.html',
   styleUrls: ['./product-delete.component.scss']
 })
-export class ProductDeleteComponent implements OnInit {
+export class ProductDeleteComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject();
 
   constructor(
     public dialogRef: MatDialogRef<ProductDeleteComponent>,
@@ -25,8 +28,12 @@ export class ProductDeleteComponent implements OnInit {
   }
 
   delete(): void {
-    this.productService.delete(this.data.product).subscribe(() => {
+    this.productService.delete(this.data.product).pipe(takeUntil(this.destroy$)).subscribe(() => {
       this.dialogRef.close();
     });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
   }
 }
--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,8 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductService } from '../product.service';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { Product } from '../product';
+import { Subject, Subscription } from 'rxjs/index';
+import { takeUntil } from 'rxjs/internal/operators';
 
 interface DialogData {
   product: Product;
@@ -13,8 +15,9 @@ interface DialogData {
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.scss']
 })
-export class ProductEditComponent implements OnInit {
+export class ProductEditComponent implements OnInit, OnDestroy {
   form: FormGroup;
+  destroy$ = new Subject();
 
   constructor(
     private fb: FormBuilder,
@@ -40,8 +43,10 @@ export class ProductEditComponent implements OnInit {
     }
 
     const newProduct: Product = new Product(this.form.get('name').value, this.form.get('price').value);
-    console.log(this.data.product);
-    console.log(newProduct);
-    this.productService.edit(this.data.product, newProduct).subscribe(() => this.dialogRef.close());
+    this.productService.edit(this.data.product, newProduct).pipe(takeUntil(this.destroy$)).subscribe(() => this.dialogRef.close());
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
   }
 }
